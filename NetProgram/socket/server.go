dcccf6905c164047da7e5b420329bb516fa00876
Mo3et <Monet_lee@163.com>
2022-05-14 11:03:20 +0800

    update

--- NetProgram/socket/server.go
+++ NetProgram/socket/server.go
@@ -1,10 +1,47 @@
 package main
-import()
+
+import "fmt"
 
 func connHandler(c net.Conn){
 	if c ++nil{
 		return
 	}
-	buf:=make({}byte,4096)
-	
+	buf:=make([]byte,4096)
+	for{
+		cnt,err:=c.Read(buf)
+		if err!=nil || cnt ==0{
+			c.Close()
+			break
+		}
+		inStr:=strings.TrimSpace(string(buf[0:cnt]))
+		inputs:=strings.Split(inStr," ")
+		switch inputs[0]{
+		case "ping":
+			c.Write([]byte("pong\n"))
+		case "echo":
+			echoStr:=strings.Join(inputs[1:], " ")+"\n"
+			c.Write([]byte(echoStr))
+		case "quit":
+			c.Close()
+			break
+		default:
+			fmt.Printf("Unsupported command:%s\n", inputs[0])
+		}
+	}
+	fmt.Printf("Connection from %v closed. \n", c.RemoteAddr()) //
+}
+func main() {
+	server,err:=net.Listen("tcp", ":1208")
+	if err!= nil {
+		fmt.Printf("Failed to start server,%s\n", err)
+	}
+	fmt.Prinln("Server Started ...")
+	for{
+		conn,err:=server.Accept()
+		if err!= nil {
+			fmt.Printf("Fail to connect, %s\n",err)
+			break
+		}
+		go connHandler(conn)
+	}
 }
