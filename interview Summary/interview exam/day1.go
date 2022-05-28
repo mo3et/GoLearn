22c08f85e18d67ff29c119f3ecb8b5db7e545c52
Mo3et <Monet_lee@163.com>
2022-05-28 22:38:50 +0800

    update

--- interview Summary/interview exam/day1.go
+++ interview Summary/interview exam/day1.go
@@ -4,16 +4,14 @@ package main
 import (
 	"fmt"
 )
+
 func main() {
 	main1()
 	main2()
 	main5()
 }
 
-
 //  Day 1
-
-
 
 func main1() {
 	defer_call()
@@ -53,7 +51,6 @@ func main2() {
 3 -> 3
 */
 
-
 // Day 3
 // 一.
 // 1.
@@ -61,20 +58,22 @@ func main3_1() {
 	s := make([]int, 5)
 	s = append(s, 1, 2, 3)
 	fmt.Println(s)
-}// Output [0 0 0 0 0 1 2 3]
+} // Output [0 0 0 0 0 1 2 3]
 
 // 2.
 func main3_2() {
-   s := make([]int,0)
-   s = append(s,1,2,3,4)
-   fmt.Println(s)
-}// Output [1 2 3 4]
+	s := make([]int, 0)
+	s = append(s, 1, 2, 3, 4)
+	fmt.Println(s)
+} // Output [1 2 3 4]
 
 // 二.
-func funcMui(x,y int)(sum int,error){
-	return x+y,nil
+
+func funcMui(x, y int) (sum int, error error) {
+	return x + y, nil
 }
- //缺陷  第二个返回值没有命名
+
+//缺陷  第二个返回值没有命名
 
 //  三.
 
@@ -89,62 +88,63 @@ func funcMui(x,y int)(sum int,error){
 // Day 4
 
 // 1.
-func main4_1(){
-	list:=new([]int)//不能用new 不然生成是 *[]int 类型的指针 不能对指针执行 append11
-	list:=append(list,1)
+func main4_1() {
+	list := new([]int) //不能用new 不然生成是 *[]int 类型的指针 不能对指针执行 append11
+	list := append(list, 1)
 	fmt.Println(list)
 }
+
 // 参考答案及解析：
 // 不能通过编译，new([]int) 之后的 list 是一个 *[]int 类型的指针，不能对指针执行 append 操作。可以使用 make() 初始化之后再用。同样的，map 和 channel 建议使用 make() 或字面量的方式初始化，不要用 new() 。
 
-// 2. 
+// 2.
 func main4_2() {
-	s1:=[]int{1,2,3}
-	s2:=[]int{4,5}
-	s1:=append(s1,s2)//需要改为s2...
+	s1 := []int{1, 2, 3}
+	s2 := []int{4, 5}
+	s1 := append(s1, s2) //需要改为s2...
 	fmt.Println(s1)
-}//不能通过编译 append 第二个参数不能直接使用slice 需要... 不定参数
-
+} //不能通过编译 append 第二个参数不能直接使用slice 需要... 不定参数
 
 // 3.
-var {
-	size:=1024//只能在函数内部使用简短模式
-	max_size=size*2
-}
+// var {
+// 	 size:=1024//只能在函数内部使用简短模式
+// 	max_size=size*2
+// }
 func main4_3() {
-	fmt.Println(size,max_size)
+	fmt.Println(size, max_size)
 }
+
 /*
 	使用限制
 1.必须使用显示初始化
 2.不能提供数据类型，编译器会自动推导
 3.只能在函数内部使用简短模式
- */
+*/
 
 //  Day 5
 func main5() {
-	sn1:=struct{
-		age int
+	sn1 := struct {
+		age  int
 		name string
-	}{age:11 ,name: "qq"}
-	sn2:=struct{
-		age int
+	}{age: 11, name: "qq"}
+	sn2 := struct {
+		age  int
 		name string
-	}{age:11,name: "qq"}
+	}{age: 11, name: "qq"}
 
-	if sn1==sn2{
+	if sn1 == sn2 {
 		fmt.Println("sn1 == sn2")
 	}
-	sm1:=struct{
-	age int
-		m map[string]string
-	}{age:11,m:map[string]string{"a":"1"}}
-	
-	sm2:=struct{
-	age int
-		m map[string]string
-	}{age:11,m:map[string]string{"a":"1"}}
-if sm1==sm2{
-	fmt.Println("sm1 == sm2")
-}	
+	sm1 := struct {
+		age int
+		m   map[string]string
+	}{age: 11, m: map[string]string{"a": "1"}}
+
+	sm2 := struct {
+		age int
+		m   map[string]string
+	}{age: 11, m: map[string]string{"a": "1"}}
+	if sm1 == sm2 {
+		fmt.Println("sm1 == sm2")
+	}
 }
