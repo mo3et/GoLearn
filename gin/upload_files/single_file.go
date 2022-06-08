ad998db9cd2bf8e5576584323aa2f2beba94a077
Mo3et <monet_lee@163.com>
2022-06-08 19:11:57 +0800

    update

---
+++ gin/upload_files/single_file.go
@@ -0,0 +1,18 @@
+func main() {
+	router := gin.Default()
+	// Set a lower memory limit ofr miltipart form (default is 32 MiB)
+	router.MaxMultipartMemory = 8 << 20 // 8 MiB
+	router.POST("/upload", func(c *gin.Context) {
+		// single file
+		file, _ := c.FormFile("file")
+		log.Println(file.Filename)
+
+		// Upload the file to specific dst.
+		c.SaveUploadedFile(file, dst)
+
+		c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
+	})
+	router.Run(":8081")
+}
+
+curl -X POST http://localhost:8080/upload \
