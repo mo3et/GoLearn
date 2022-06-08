79022113efccfe08922f034d7c9a1a993b1f06b5
Mo3et <monet_lee@163.com>
2022-06-08 21:10:59 +0800

    update

---
+++ gin/Cust_Middleware.go
@@ -0,0 +1,35 @@
+func Logger() gin.HandlerFUnc {
+	return func(c *gin.Context) {
+		t := time.Now()
+
+		// Set example variable
+		c.Set("example", "12345")
+
+		// before request
+
+		c.Next()
+
+		// after request
+		latency := time.Since(t)
+		log.Print(latency)
+
+		// access the status we are sending
+		status := c.Writer.Status()
+		log.Println(status)
+	}
+}
+
+func main() {
+	r := gin.New()
+	r.Use(Logger())
+
+	r.GET("/test", func(c *gin.Context) {
+		example := c.MustGet("example").(string)
+
+		// it would print: "12345"
+		log.Println(example)
+	})
+
+	// Listen ddand serve on 0.0.0.0:8080
+	r.Run(":8081")
+}
