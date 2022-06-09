796331c2de1e250416b2d03440df531ff1dae41e
Mo3et <monet_lee@163.com>
2022-06-09 22:11:01 +0800

    update

--- test-exam.go
+++ test-exam.go
@@ -1,27 +1,44 @@
 package main
 
 import (
-    "fmt"
-    "time"
+	"fmt"
 )
 
+// func main() {
+//     // 合起来写
+//     go func() {
+//         i := 0
+//         for {
+//             i++
+//             fmt.Printf("new goroutine: i = %d\n", i)
+//             time.Sleep(time.Second)
+//         }
+//     }()
+//     i := 0
+//     for {
+//         i++
+//         fmt.Printf("main goroutine: i = %d\n", i)
+//         time.Sleep(time.Second)
+//         if i == 20 {
+//             break
+//         }
+//     }
+// }
+
+// 用goroutine去接收 channal value
+func recv(c chan int) {
+	ret := <-c
+	fmt.Println("接收成功", ret)
+}
 func main() {
-    // 合起来写
-    go func() {
-        i := 0
-        for {
-            i++
-            fmt.Printf("new goroutine: i = %d\n", i)
-            time.Sleep(time.Second)
-        }
-    }()
-    i := 0
-    for {
-        i++
-        fmt.Printf("main goroutine: i = %d\n", i)
-        time.Sleep(time.Second)
-        if i == 20 {
-            break
-        }
-    }
+	ch := make(chan int)
+	go recv(ch) //启用go Routine
+	ch <- 10
+	// num := <-ch
+	// select {
+	// case <-ch:
+	// 	// ch <- 10
+	// 	fmt.Println("Great.", num)
+	// }
+	fmt.Println("Send Success.")
 }
