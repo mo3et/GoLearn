ed6ad6feba102a697e3633e88dd248865c996ce8
Mo3et <monet_lee@163.com>
2022-06-10 18:12:33 +0800

    update

--- the-way-to-go/bit-manipulation.go
+++ the-way-to-go/bit-manipulation.go
@@ -54,3 +54,29 @@
 
 // 使用位左移与iota计数实现存储单位的常量枚举：
 
+type ByteSize float64
+
+const (
+	_           = iota //通过赋值给空白标识符来忽略值
+	KB ByteSize = 1 << (10 * iota)
+	MB
+	GB
+	TB
+	PB
+	EB
+	ZB
+	YB
+)
+
+// 通讯中使用左移表示标识的example
+
+type BitFlag int
+const (
+	Active  BitFlag = 1 << iota // 1 << 0 ==1
+	Send                        //1 << 1==2
+	Receive                     //1 <<2 ==4
+)
+
+flag := Active | Send // ==3
+
+
