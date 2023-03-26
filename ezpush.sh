dc9d0e92a343348984f11c1a1b8432d8f90cc684
Monet Lee <monet_lee@163.com>
2023-03-26 19:47:57 +0800

    update

--- ezpush.sh
+++ ezpush.sh
@@ -2,9 +2,8 @@
 # echo $(git add -A)
 # # echo $(git commit -am "update")
 # echo $(git commit -m "update")
-# sleep 1s
 # echo $(git push --verbose)
-# # echo $(git add -A && git commit -am update && git push)
+# # echo $(git add -A && git commit -am "update" && git push --verbose)
 
 git add -A
 git commit -am "update"
